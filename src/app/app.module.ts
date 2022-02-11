319be940b4fa9f9225d1fa7105f63e2336590320
prisciladuarte <duartecostap@gmail.com>
2022-02-11 16:08:14 -0300

    Adiciona estilização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeModule } from '@shared/modules/prime.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    PrimeModule,
     BrowserModule,
     AppRoutingModule
   ],
