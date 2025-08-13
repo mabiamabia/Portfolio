fa5c194c4d9530a317398f5f6f5aa05ca7b77e56
priscila.costa <priscila.costa@g.globo>
2025-08-12 21:57:45 -0300

    feat: adiciona componente de projetos

--- src/app/shared/helpers/projects.ts
+++ src/app/shared/helpers/projects.ts
@@ -8,16 +8,9 @@ export interface Projects {
 }
 
 export const projectsMock: Projects[] = [
+
   {
     id: 1,
-    title: "E-commerce Vem da Terra",
-    description: "Aplicativo de comércio eletrônico com carrinho de compras e integração de pagamento.",
-    technologies: ["Spring Boot", "Java", "MySQL", "Angular"],
-    projectUrl: "https://vemdaterrabrasil.netlify.app/#/inicio",
-    githubUrl: "https://github.com/mabiamabia/E-commerce-Vem-da-Terra"
-  },
-  {
-    id: 2,
     title: "Spotplay API",
     description: "API que simula o Spotify, permitindo integração e acesso a dados de música.",
     technologies: ["Node.js", "Express", "API Rest", "Docker"],
@@ -25,7 +18,7 @@ export const projectsMock: Projects[] = [
     githubUrl: "https://github.com/mabiamabia/spotplay-API-que-simula-spotify"
   },
   {
-    id: 3,
+    id: 2,
     title: "TelaFlix",
     description: "Simulação de uma página inicial de streaming.",
     technologies: ["React", "CSS", "Javascript"],
@@ -33,11 +26,19 @@ export const projectsMock: Projects[] = [
     githubUrl: "https://github.com/mabiamabia/TelaFlix"
   },
   {
-    id: 4,
+    id: 3,
     title: "Portfolio Website",
     description: "Um site de portfólio responsivo para mostrar meus projetos e habilidades.",
     technologies: ["Angular", "TypeScript"],
     projectUrl: "https://prisciladuarte.netlify.app/#/",
     githubUrl: "https://github.com/mabiamabia/Portfolio"
-  }
+  },
+    {
+    id: 4,
+    title: "E-commerce Vem da Terra",
+    description: "Aplicativo de comércio eletrônico com carrinho de compras e integração de pagamento.",
+    technologies: ["Spring Boot", "Java", "MySQL", "Angular"],
+    projectUrl: "https://vemdaterrabrasil.netlify.app/#/inicio",
+    githubUrl: "https://github.com/mabiamabia/E-commerce-Vem-da-Terra"
+  },
 ];
